bce02d715a47c0fd15cfd0dbfbef2bf2cba136cf
theripper93 <tsplab@gmail.com>
2021-08-22 00:14:47 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,68 @@
 Hooks.on("renderCombatTracker", (app, html, data) => {
-    $("#combat-popout").find("#combat-tracker").animate({
-        scrollLeft: $("#combat-popout").find(".combatant.active")[0]?.getBoundingClientRect()?.x - $("#combat-popout")[0]?.getBoundingClientRect()?.x - $("#combat-popout")[0]?.getBoundingClientRect()?.width/2
-    }, 0)
-    if(html[0].id != "combat-popout") return
-    html.find()
-    app.position.top = 10
-    app.position.left = 120
-    html.css({
-        top: "10px",
-        left: "120px"
-    })
+  $("#combat-popout")
+    .find("#combat-tracker")
+    .animate(
+      {
+        scrollLeft:
+          ($("#combat-popout")
+            .find(".combatant.active")[0]
+            ?.getBoundingClientRect()?.x -
+            $("#combat-popout")[0]?.getBoundingClientRect()?.x -
+            $("#combat-popout")[0]?.getBoundingClientRect()?.width / 2) /
+          game.settings.get("horizontal-combat", "scale"),
+      },
+      0
+    );
+  if (html[0].id != "combat-popout") return;
+  html.find();
+  app.position.top = 10;
+  app.position.left = 120;
+  html.css({
+    top: "10px",
+    left: "120px",
+  });
+});
 
-})
+Hooks.on("createCombat", () => {
+  ui.combat.renderPopout();
+});
 
-Hooks.on("createCombat",() => {
-    ui.combat.renderPopout()
-})
-
-Hooks.on("canvasReady",() => {
-    if(game?.combat?.started) {
-        ui.combat.renderPopout()
-    }else{
-        ui.combat?._popout?.close()
-    }
-})
+Hooks.on("canvasReady", () => {
+  if (game?.combat?.started) {
+    ui.combat.renderPopout();
+  } else {
+    ui.combat?._popout?.close();
+  }
+});
 
 Hooks.on("deleteCombat", () => {
-    ui.combat?._popout?.close()
+  ui.combat?._popout?.close();
+});
+
+Hooks.on("collapseSidebar",(sidebar,collapsed)=>{
+  document.documentElement.style.setProperty("--hc-sidebar", collapsed ? "50px" : "330px");
 })
+
+Hooks.on("init", () => {
+  game.settings.register("horizontal-combat", "scale", {
+    name: "Scale",
+    hint: "Scale of the Horizontal Combat Tracker",
+    scope: "client",
+    config: true,
+    type: Number,
+    range: {
+      min: 0.1,
+      max: 2,
+      step: 0.05,
+    },
+    default: 1,
+    onChange: (sett) => {
+      document.documentElement.style.setProperty("--hc-scale", sett);
+    },
+  });
+
+  document.documentElement.style.setProperty(
+    "--hc-scale",
+    game.settings.get("horizontal-combat", "scale")
+  );
+});
