0498c35eec7cc64127cb1910321fce8ea385dcca
theripper93 <tsplab@gmail.com>
2021-08-22 04:16:03 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -13,39 +13,47 @@ Hooks.on("renderCombatTracker", (app, html, data) => {
       },
       0
     );
+  if (game.settings.get("horizontal-combat", "scale") < 1)
+    $("#combat-popout")?.css({
+      top: `10px`,
+      left: `120px`,
+    });
   if (html[0].id != "combat-popout") return;
   html.find();
   app.position.top = 10;
   app.position.left = 120;
   html.css({
-    top: "10px",
-    left: "120px",
+    top: `10px`,
+    left: `120px`,
   });
 });
 
 Hooks.on("createCombat", () => {
   ui.combat.renderPopout();
-  ui.nav.collapse()
+  ui.nav.collapse();
 });
 
 Hooks.on("canvasReady", () => {
   if (game?.combat?.started) {
     ui.combat.renderPopout();
-    ui.nav.collapse()
+    ui.nav.collapse();
   } else {
     ui.combat?._popout?.close();
-    ui.nav.expand()
+    ui.nav.expand();
   }
 });
 
 Hooks.on("deleteCombat", () => {
   ui.combat?._popout?.close();
-  ui.nav.expand()
+  ui.nav.expand();
 });
 
-Hooks.on("collapseSidebar",(sidebar,collapsed)=>{
-  document.documentElement.style.setProperty("--hc-sidebar", collapsed ? "50px" : "330px");
-})
+Hooks.on("collapseSidebar", (sidebar, collapsed) => {
+  document.documentElement.style.setProperty(
+    "--hc-sidebar",
+    collapsed ? "50px" : "330px"
+  );
+});
 
 Hooks.on("init", () => {
   game.settings.register("horizontal-combat", "scale", {
